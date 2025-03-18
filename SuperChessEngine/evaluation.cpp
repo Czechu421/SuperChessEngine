d96977ef6e477209b48805b03a2689185a452b02
Czechu <117066782+Czechu421@users.noreply.github.com>
2025-03-18 12:06:06 +0000

    Preapered space for new evaluation

--- SuperChessEngine/evaluation.cpp
+++ SuperChessEngine/evaluation.cpp
@@ -5,19 +5,15 @@
 using namespace chess;
 
 static INT evaluate(const Board& board) {
+INT score = 0;
+
 const INT queenWeight = 1000;
 const INT rookWeight = 525;
 const INT bishopWeight = 350;
 const INT knightWeight = 350;
 const INT pawnWeight = 100;
 
-INT materialScore = queenWeight		* (board.pieces(PieceType::QUEEN, Color::WHITE).count()		- board.pieces(PieceType::QUEEN, Color::BLACK).count())
-				  + rookWeight		* (board.pieces(PieceType::ROOK, Color::WHITE).count()		- board.pieces(PieceType::ROOK, Color::BLACK).count())
-				  + bishopWeight	* (board.pieces(PieceType::BISHOP, Color::WHITE).count()	- board.pieces(PieceType::BISHOP, Color::BLACK).count())
-				  + knightWeight	* (board.pieces(PieceType::KNIGHT, Color::WHITE).count()	- board.pieces(PieceType::KNIGHT, Color::BLACK).count())
-				  + pawnWeight		* (board.pieces(PieceType::PAWN, Color::WHITE).count()		- board.pieces(PieceType::PAWN, Color::BLACK).count());
-
-INT score = materialScore;
+// NEW EVALUATION HERE
 
 if (board.sideToMove()) {
 	score *= -1;
